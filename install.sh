e250c20fd9211af467295aaaf19d6c2025ec15ae
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2017-04-22 18:47:40 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,60 @@
 #!/bin/bash
 
-# require: pip, curl, git
-pip install virtualenv virtualenvwrapper
-curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
+if type git > /dev/null 2&1; then
+  brew install git
+  # apt-get -y install git
+  # yum -y install git
+fi
+# install pythonz
+if type curl > /dev/null 2&1; then
+  curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
+  pythonz list -a
+  pythonz install 3.3.5
 
-# pythonz list -a
-# pythonz install 3.3.5
+fi
 
-# vim ~/.bashrc
-source /usr/local/bin/virtualenvwrapper.sh
+# require: pip, curl
+if type pip > /dev/null 2>&1; then
+  pip install virtualenv virtualenvwrapper
+  source /usr/local/bin/virtualenvwrapper.sh
+  mkvirtualenv -p .pythonz/pythons/CPython-3.3.5/bin/python py335
+fi
 
-# mkvirtualenv -p .pythonz/pythons/CPython-3.3.5/bin/python py335
+# install zsh
+ZSH_VERSION=5.3.1
+if type wget > /dev/null 2>&1; then
+  wget https://sourceforge.net/projects/zsh/files/zsh/${ZSH_VERSION}/zsh-${ZSH_VERSION}.tar.gz/download
+  tar zxf download
+  cd zsh zsh-${ZSH_VERSION}
+  ./configure && make && sudo make install
+fi
 
+# install some module related with rbenv
+if type git > /dev/null 2>&1; then
+  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+  git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
 
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+# install neobundle
+if type git > /dev/null 2>&1; then
+  git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
 
-brew update
-brew install ack pt tig mycli
-gem install gist
+# install with brew
+if type brew > /dev/null 2>&1; then
+  brew update
+  brew install ack pt tig mycli
+fi
 
+if type gem > /dev/null 2>&1; then
+  gem install gist
+fi
+
+# install dotfiles
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
 
-    ln -s -b $f ~/$f
+    ln -sb $(pwd)/$f ~/$f
 done
